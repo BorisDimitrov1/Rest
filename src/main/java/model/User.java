e7a6e13f8d62787119eaa693cd7b26374a708e4d
boris <b.dimitrov1@abv.bg>
2024-10-13 22:29:55 +0300

    Rest Assured Task

---
+++ src/main/java/model/User.java
@@ -0,0 +1,58 @@
+package model;
+
+public class User {
+
+    private String name;
+    private String job;
+
+    private String updatedAt;
+
+    private String id;
+    private String createdAt;
+
+
+    public User(String name, String job) {
+        this.name = name;
+        this.job = job;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+}
---
+++ src/test/java/tests/UserApiTest.java
@@ -0,0 +1,114 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import model.User;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class UserApiTest {
+
+    private static final String USERS_END_POINT = "/users";
+    private static final String GET_USERS_LIST = USERS_END_POINT + "?page=2";
+
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = System.getProperty("BaseURL");
+    }
+
+    @Test
+    public void getUsersList() {
+        Response response = RestAssured.given()
+                .when()
+                .get(GET_USERS_LIST)
+                .then()
+                .extract()
+                .response();
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+        Assert.assertNotNull(response.jsonPath().get("data"));
+        Assert.assertTrue(response.jsonPath().getList("data").size() > 0);
+    }
+
+    @Test
+    public void createUser() {
+        User user = new User("Georgi", "Banker");
+
+        Response response = RestAssured.given()
+                .contentType("application/json")
+                .body(user)
+                .when()
+                .post(USERS_END_POINT)
+                .then()
+                .extract()
+                .response();
+
+        Assert.assertEquals(response.getStatusCode(), 201);
+        Assert.assertEquals(response.jsonPath().get("name"), "Georgi");
+        Assert.assertEquals(response.jsonPath().get("job"), "Banker");
+        Assert.assertNotNull(response.jsonPath().get("id"));
+        Assert.assertNotNull(response.jsonPath().get("createdAt"));
+    }
+
+    /**
+     * Normally I would implement the negative cases with missing name or job.
+     * Too long name or job, special characters in the name or job. Field that is not expected, malformed json
+     * Wrong contentType, empty body etc... but the user is being created no matter what
+     * This is the only way for me to get response other than 201 - no content type and no body
+     */
+    @Test
+    public void createUserWithoutContentTypeAndBody() {
+        Response response = RestAssured.given()
+                .when()
+                .post(USERS_END_POINT)
+                .then()
+                .extract()
+                .response();
+
+        Assert.assertEquals(response.getStatusCode(), 415);
+        Assert.assertEquals(response.getStatusLine(), "HTTP/1.1 415 Unsupported Media Type");
+    }
+
+
+    @Test
+    public void updateUser() {
+        User user = new User("Georgi", "Cleaning guy");
+
+
+        //Create the user that is going to be updated and get the id
+        Response response = RestAssured.given()
+                .contentType("application/json")
+                .body(user)
+                .when()
+                .post(USERS_END_POINT)
+                .then()
+                .extract()
+                .response();
+
+        user.setId(response.jsonPath().get("id"));
+
+
+        //Update the user
+        response = RestAssured.given()
+                .contentType("application/json")
+                .body(user)
+                .when()
+                .put(USERS_END_POINT + "/" + user.getId())
+                .then()
+                .extract()
+                .response();
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+        Assert.assertEquals(response.jsonPath().get("name"), "Georgi");
+        Assert.assertEquals(response.jsonPath().get("job"), "Cleaning guy");
+        Assert.assertNotNull(response.jsonPath().get("updatedAt"));
+    }
+
+
+    /**
+     * Again the negative cases for PUT /users/{id} would be - id that is not created, missing name or job.
+     * Too long name or job,special characters in the name or job. Field that is not expected, malformed json
+     * Wrong contentType, empty body etc... but the user is being updated no matter what
+     */
+}
